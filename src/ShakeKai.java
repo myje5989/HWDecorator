45932d348df126d9c2d8082c59d2335ef7d7fe80
myje5989 <30823860+myje5989@users.noreply.github.com>
2017-09-05 14:46:31 +0700

    $ to ฿

--- src/ShakeKai.java
+++ src/ShakeKai.java
@@ -3,22 +3,22 @@ public class ShakeKai {
 Menu beverage;
 	public static void main(String[] args) {
 		Menu b1 = new DrumstickChicken();
-		System.out.println(b1.getDescription() +  " $ "+ b1.cost());
+		System.out.println(b1.getDescription() +  " ß "+ b1.cost());
 		
 		Menu b2 = new CheeseBall();
 		b2 = new Spicy(b2);
 		b2 = new Bbq(b2);
-		System.out.println(b2.getDescription() +  " $ "+ b2.cost());
+		System.out.println(b2.getDescription() +  " ß "+ b2.cost());
 		
 		Menu b3 = new Frenfry();
 		b3 = new Paprika(b3);
 		b3 = new Bbq(b3);
 		b3 = new Cheese(b3);
-		System.out.println(b3.getDescription() +  " $ "+ b3.cost());
+		System.out.println(b3.getDescription() +  " ß "+ b3.cost());
 		Menu b4 = new KaiPop();
 		b4 = new Spicy(b4);
 		b4 = new Cheese(b4);
-		System.out.println(b4.getDescription() +  " $ "+ b4.cost());
+		System.out.println(b4.getDescription() +  " ß "+ b4.cost());
 		
 	}
 
